dc3c4412a72359e96ccf153cfe576b2f9fcd4e51
co6tter <34955642+co6tter@users.noreply.github.com>
2025-01-29 18:18:26 +0900

    chore: add window resize

--- js/index.js
+++ js/index.js
@@ -5,7 +5,20 @@ $("#js-button-drawer").on("click", function () {
 });
 
 $("#js-drawer a").on("click", function () {
-  $("#js-button-drawer").removeClass("is-checked");
-  $("#js-drawer").slideToggle();
-  $("body").removeClass("is-fixed");
+  if (window.innerWidth < 768) {
+    $("#js-button-drawer").removeClass("is-checked");
+    $("#js-drawer").slideToggle();
+    $("body").removeClass("is-fixed");
+  }
+});
+
+$(window).on("resize", function () {
+  if (window.innerWidth >= 768) {
+    $("#js-drawer").show();
+    $("body").removeClass("is-fixed");
+  } else {
+    if (!$("#js-button-drawer").hasClass("is-checked")) {
+      $("#js-drawer").hide();
+    }
+  }
 });
